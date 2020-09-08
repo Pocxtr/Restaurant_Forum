5ef5d568e6b7655b42e364ded043beab965e8553
Wei <pocxtr@gmail.com>
2020-09-08 18:23:58 +0800

    Add admin/user index page

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
-app.engine('handlebars', handlebars()) //Handlebars 註冊樣板引擎
-app.set('view engine', 'handlebars') //設定使用Handlebars 作為樣板引擎
+
+app.engine('hbs', handlebars({ defaultLayout: 'main', extname: 'hbs'})) //Handlebars 註冊樣板引擎
+app.set('view engine', 'hbs') //設定使用Handlebars 作為樣板引擎
 
 app.listen(port, () => {
-    console.log(`example app listening on port ${port}!`)
+    console.log(`Example app listening on port ${port}!`)
 })
+
+require('./routes')(app)
