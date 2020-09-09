dee9729b3d12047f32e458e680d82f7e537cd572
Wei <pocxtr@gmail.com>
2020-09-09 16:45:19 +0800

    Add userController & signup page

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 const db = require('./models')
 const app = express()
 const port = 3000
 
 app.engine('hbs', handlebars({ defaultLayout: 'main', extname: 'hbs'})) //Handlebars 註冊樣板引擎
 app.set('view engine', 'hbs') //設定使用Handlebars 作為樣板引擎
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(port, () => {
     db.sequelize.sync()
