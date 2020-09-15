85571fb7c718840cbaf7202436f46bbb2a08bdd9
Wei <pocxtr@gmail.com>
2020-09-15 16:40:00 +0800

    Listen to process.env.PORT

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const passport = require('./config/passport')
 const methodOverride = require('method-override')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.engine('hbs', handlebars({ defaultLayout: 'main', extname: 'hbs'})) //Handlebars 註冊樣板引擎
 app.set('view engine', 'hbs') //設定使用Handlebars 作為樣板引擎
