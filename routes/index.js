5874a27c84e5c56fd86f229479f6078a103e3aa6
Wei <pocxtr@gmail.com>
2020-09-20 23:14:05 +0800

    Add addFollowing and removeFollowing to userController

--- routes/index.js
+++ routes/index.js
@@ -35,7 +35,8 @@ module.exports = (app, passport) => {
   app.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
   app.post('/like/:restaurantId', authenticated, userController.addLike)
   app.delete('/like/:restaurantId', authenticated, userController.removeLike)
-
+  app.post('/following/:userId', authenticated, userController.addFollowing)
+  app.delete('/following/:userId', authenticated, userController.removeFollowing)
 
   app.get('/users/top', authenticated, userController.getTopUser)
   app.get('/users/:id', authenticated, userController.getUser)
