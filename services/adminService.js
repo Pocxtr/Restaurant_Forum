5237b11f22c6506b8acbd1874e0b3dc9d823e911
Wei <pocxtr@gmail.com>
2020-09-22 09:20:53 +0800

    GET /api/admin/restaurants/:id

--- services/adminService.js
+++ services/adminService.js
@@ -7,7 +7,13 @@ const adminService = {
     return Restaurant.findAll({ raw: true, nest:true, include: [Category] }).then(restaurants => {
       callback({ restaurants: restaurants })
     })
-  }
+  },
+  getRestaurant : (req, res, callback) => {
+    return Restaurant.findByPk(req.params.id, { raw: true, nest: true, include:[Category] })
+    .then(restaurant => {
+        callback({ restaurant: restaurant })
+    })
+}
 }
 
 module.exports = adminService
